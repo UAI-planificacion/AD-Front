e86ffd7406344796b787ebaf7e2cd385211c17ee
kevinkeyssx <keyss1700@gmail.com>
2026-06-24 20:11:07 -0400

    vigentes endpoint implemented

--- app/api/ads/get-all/route.ts
+++ app/api/ads/get-all/route.ts
@@ -7,20 +7,25 @@ import type { Publicidad }      from '@/lib/models/ads';
 import { ENV }                  from '@/config/envs/env';
 
 
-export async function GET(): Promise<NextResponse> {
-    try {
-        const data = await connectRequest<Publicidad[]>({
-            endpoint     : `${ ENV.RESERVAS.API_URL }/${ EXTERNAL_ENDPOINT.ADS.GET_ALL }`,
-            method       : METHOD.GET,
-            isInternal   : false,
-            extraHeaders : {
-                Authorization : `Key ${ ENV.RESERVAS.API_KEY }`,
-            },
-        });
+export async function GET( request : Request ) : Promise<NextResponse> {
+	try {
+		const { searchParams } = new URL( request.url );
+		const isVigent = searchParams.get( 'vigentes' ) === 'true';
 
-        return NextResponse.json( data );
-    } catch ( error: unknown ) {
-        const message = error instanceof Error ? error.message : 'Error al obtener publicidades';
-        return NextResponse.json({ message }, { status: 500 });
-    }
+		const endpoint = isVigent ? EXTERNAL_ENDPOINT.ADS.VIGENT : EXTERNAL_ENDPOINT.ADS.GET_ALL;
+
+		const data = await connectRequest<Publicidad[]>( {
+			endpoint		: `${ ENV.RESERVAS.API_URL }/${ endpoint }`,
+			method			: METHOD.GET,
+			isInternal		: false,
+			extraHeaders	: {
+				Authorization	: `Key ${ ENV.RESERVAS.API_KEY }`,
+			},
+		} );
+
+		return NextResponse.json( data );
+	} catch ( error : unknown ) {
+		const message = error instanceof Error ? error.message : 'Error al obtener publicidades';
+		return NextResponse.json( { message }, { status : 500 } );
+	}
 }
